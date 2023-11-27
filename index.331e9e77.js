3380c4a740c035a91c54713608fd75e5ca1f3d48
danretegan <96877914+danretegan@users.noreply.github.com>
2023-11-27 07:11:53 +0000

    Deploying to gh-pages from @ danretegan/photo-search-load-more-button@b59d6d2d6ab72091ef5bd83c6a948241c89738eb 🚀

---
+++ index.331e9e77.js
@@ -0,0 +1,2 @@
+let o=document.getElementById("btn-back-to-top");window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"},o.addEventListener("click",(function(){document.body.scrollTop=0,document.documentElement.scrollTop=0}));
+//# sourceMappingURL=index.331e9e77.js.map
